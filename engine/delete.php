c91d440abf47757677231ba739788c86ffb0a874
MishaWhite1236 <blackhole2013@icloud.com>
2022-07-19 13:46:49 +0300

    the initial edition

--- engine/delete.php
+++ engine/delete.php
@@ -3,11 +3,11 @@ if(isset($_POST["id"]))
 {
     try {
         $conn = new PDO("mysql:host=localhost;dbname=test6", "root", "root");
-        $sql = "DELETE FROM leads WHERE id = :id";
+        $sql = "DELETE FROM `lead` WHERE id = :id";
         $stmt = $conn->prepare($sql);
-        $stmt->bindValue(":userid", $_POST["id"]);
+        $stmt->bindValue(":id", $_POST["id"]);
         $stmt->execute();
-        header("Location: index.php");
+        header("Location: ../index.php");
     }
     catch (PDOException $e) {
         echo "Database error: " . $e->getMessage();
